6c59cd2ccd57afb6b27dc6e7b1819d4b2e728255
Prakhar Gupta <prakharyologupta@gmail.com>
2025-01-23 22:44:30 +0530

    cors fix

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const aiRoutes = require("./routes/aiRoutes");
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use("/api/ai", aiRoutes);
 
