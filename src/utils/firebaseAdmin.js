6aed4dabb3c6e740cac833b010fe6dda273595b2
Prakhar Gupta <prakharyologupta@gmail.com>
2025-01-25 01:26:32 +0530

    auth added

--- src/utils/firebaseAdmin.js
+++ src/utils/firebaseAdmin.js
@@ -1,6 +1,7 @@
 const admin = require("firebase-admin");
+require("dotenv").config();
 
-const serviceAccount = require("../../service-account-key.json");
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
